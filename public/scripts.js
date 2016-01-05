a737d37d23c2cfbd575b730771985bdc401d8f28
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2016-01-05 16:38:53 +0100

    Finishes with the JS part of the header

--- public/scripts.js
+++ public/scripts.js
@@ -10,11 +10,11 @@ document.addEventListener("DOMContentLoaded", function() {
 
   links.forEach(function(element) {
     element.addEventListener("mouseover", function() {
-      linkText.textContent = "I Love You!";
       linkText.style.top = this.getBoundingClientRect().top - 18 + "px";
       linkText.style.opacity = 1;
       linkText.style.animationName = "link-animation";
       linkText.style.animationDuration = "0.5s";
+      linkText.textContent = this.textContent;
     })
 
     element.addEventListener("mouseout", function() {
