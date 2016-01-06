321e7889a70ff11d10d5c17e6ad6dd5090bf66eb
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2016-01-06 12:03:17 +0100

    Doing some JS

--- public/scripts.js
+++ public/scripts.js
@@ -2,6 +2,9 @@ document.addEventListener("DOMContentLoaded", function() {
 
   var linksNodeList = document.getElementById("navigation").getElementsByTagName("li");
   var linkText = document.getElementById("navigation").getElementsByTagName("p")[0];
+  var header = document.getElementById("header-section");
+  var logo = document.getElementById("logo");
+  var mouse = document.getElementById("mouse");
   var links = [];
 
   for (var i = 0; i < linksNodeList.length; i++) {
@@ -10,7 +13,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
   links.forEach(function(element) {
     element.addEventListener("mouseover", function() {
-      linkText.style.top = this.getBoundingClientRect().top - 18 + "px";
+      linkText.style.top = this.getBoundingClientRect().top + window.scrollY - 18 + "px";
       linkText.style.opacity = 1;
       linkText.style.animationName = "link-animation";
       linkText.style.animationDuration = "0.5s";
@@ -22,4 +25,25 @@ document.addEventListener("DOMContentLoaded", function() {
       linkText.style.opacity = 0;
     })
   });
+
+  window.onscroll = function() {
+    if (window.scrollY >= convertPercentage("0%") && window.scrollY <= convertPercentage("100%")) {
+      window.requestAnimationFrame(updateParallax);
+    }
+  }
+
+  function updateParallax() {
+    var slowScroll = -window.scrollY / 2;
+    var opacityValue = 1 - (window.scrollY / convertPercentage("85%"));
+
+    logo.style.transform = "translateY(" + slowScroll + "px)"
+    mouse.style.animationIterationCount = "0";
+    mouse.style.opacity = opacityValue;
+  }
+
+  function convertPercentage(value) {
+    if (typeof(value) === "string") { value = parseFloat(value.replace("%", "")) }
+
+    return window.innerHeight * value / 100
+  }
 });
